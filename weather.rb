fda1d059b1c83d3bc025daa88b511dcc583c42d6
jessejkwk <janderson@nyatoday.com>
2018-06-21 18:10:29 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -22,6 +22,7 @@ end
 puts "The weather in #{city} is #{description}"
 
 
+
 #4. Write a method that takes in the weather hash as an argument. 
 # You'll want to use the each method to iterate over the hash and print out
 # `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
