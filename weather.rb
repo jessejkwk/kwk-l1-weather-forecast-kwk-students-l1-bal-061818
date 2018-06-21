95cb1744ca44872881d20d37472388840e160e92
jessejkwk <janderson@nyatoday.com>
2018-06-21 18:13:23 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -20,6 +20,7 @@ end
 def temperature(weather)
   weather[:temperature_farenheit]
 end
+
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
 
 def final
@@ -35,5 +36,6 @@ end
 
 # Hint - how can we find the algorithm to convert Farenheit to Celsius?
 
+final 
 
 
