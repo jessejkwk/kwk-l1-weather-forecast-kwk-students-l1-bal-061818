2e01b637c1aa9e0366b1b2d56dc7074317b0d115
jessejkwk <janderson@nyatoday.com>
2018-06-21 18:42:34 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -17,11 +17,6 @@ def description(weather)
   return weather[:description]
 end
 
-def weather_report(weather)
-  return "The weather in #{location(weather)} is #{description(weather)}. The temperature is #{:temperature_farenheit}."
-end 
-
-
 #4. Write a method that takes in the weather hash as an argument. 
 # You'll want to use the each method to iterate over the hash and print out
 # `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
